8255ff0c101a3c19f1f2ca67f2450cfe0057be66
Peter Rúček <rucekpeter2000@hotmail.com>
2021-03-05 15:44:29 +0100

    Add initial migration

---
+++ ICSproj/Migrations/20210305140651_InitialMigration.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ICSproj.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bands",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Origin = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescriptionLong = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhotoEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SrcPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BandEntityId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    StageEntityId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhotoEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PhotoEntity_Bands_BandEntityId",
+                        column: x => x.BandEntityId,
+                        principalTable: "Bands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PhotoEntity_Stages_StageEntityId",
+                        column: x => x.StageEntityId,
+                        principalTable: "Stages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Program",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PerformanceDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PerformanceDuration = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Program", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Program_Bands_BandId",
+                        column: x => x.BandId,
+                        principalTable: "Bands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Program_Stages_StageId",
+                        column: x => x.StageId,
+                        principalTable: "Stages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhotoEntity_BandEntityId",
+                table: "PhotoEntity",
+                column: "BandEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhotoEntity_StageEntityId",
+                table: "PhotoEntity",
+                column: "StageEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Program_BandId",
+                table: "Program",
+                column: "BandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Program_StageId",
+                table: "Program",
+                column: "StageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhotoEntity");
+
+            migrationBuilder.DropTable(
+                name: "Program");
+
+            migrationBuilder.DropTable(
+                name: "Bands");
+
+            migrationBuilder.DropTable(
+                name: "Stages");
+        }
+    }
+}
