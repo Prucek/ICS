105b7ed84094f9928c84889cd214e3d149520916
Peter Rúček <rucekpeter2000@hotmail.com>
2021-03-05 15:42:09 +0100

    Add DbContext

---
+++ ICSproj/DbContext/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ICSproj
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<FestivalDbContext>
+    {
+        public FestivalDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FestivalDbContext>();
+            builder.UseSqlServer(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;
+                Initial Catalog = TasksDB;
+                MultipleActiveResultSets = True;
+                Integrated Security = True; ");
+            return new FestivalDbContext(builder.Options);
+        }
+    }
+}
---
+++ ICSproj/DbContext/FestivalDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ICSproj.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICSproj
+{
+    public class FestivalDbContext : DbContext
+    {
+        public FestivalDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BandEntity> Bands { get; set; }
+        public DbSet<StageEntity> Stages { get; set; }
+        public DbSet<ScheduleEntity> Program { get; set; }
+    }
+}
