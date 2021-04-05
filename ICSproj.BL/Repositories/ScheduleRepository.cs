49114604c15d57d24ae579d340a06b384dafafe4
Peter Rúček <rucekpeter2000@hotmail.com>
2021-04-05 12:56:55 +0200

    Change ICSproj.DAL direcotry

---
+++ ICSproj.BL/Repositories/ScheduleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSproj.BL.Repositories
+{
+    class ScheduleRepository
+    {
+    }
+}
