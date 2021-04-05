7d296718db542500270d3c80c67f93e47df8c759
Peter Rúček <rucekpeter2000@hotmail.com>
2021-04-05 13:06:19 +0200

    Reslove project directories

---
+++ ICSproj.BL.Tests/ScheduleRepositoryTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ICSproj.BL.Tests
+{
+    public class ScheduleRepositoryTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
