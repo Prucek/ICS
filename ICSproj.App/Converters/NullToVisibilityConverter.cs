0f0c021a995ac537ffafa6140aeb86b1d4d7d669
Peter Rúček <rucekpeter2000@hotmail.com>
2021-05-07 23:01:20 +0200

    Merge

--- ICSproj.App/Converters/NullToVisibilityConverter.cs
+++ ICSproj.App/Converters/NullToVisibilityConverter.cs
@@ -13,7 +13,7 @@ namespace ICSproj.App.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null ? Visibility.Hidden : Visibility.Visible;
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
